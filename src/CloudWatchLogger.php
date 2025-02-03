299cfd3db5702eaaf3c3ee55d505d12a26c6ee4a
MolanoAlternova <santiago.molano@alternova.com>
2025-02-03 11:44:50 -0500

    Add exceptions

--- src/CloudWatchLogger.php
+++ src/CloudWatchLogger.php
@@ -5,6 +5,7 @@ namespace Molxno\LaravelCloudwatch;
 use Aws\CloudWatchLogs\CloudWatchLogsClient;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
+use Aws\Exception\AwsException;
 
 class CloudWatchLogger extends AbstractProcessingHandler
 {
@@ -17,17 +18,21 @@ class CloudWatchLogger extends AbstractProcessingHandler
     {
         parent::__construct($level, $bubble);
 
-        $this->client = new CloudWatchLogsClient([
-            'version'     => 'latest',
-            'region'      => $config['region'],
-            'credentials' => [
-                'key'    => $config['key'],
-                'secret' => $config['secret'],
-            ],
-        ]);
+        try {
+            $this->client = new CloudWatchLogsClient([
+                'version'     => 'latest',
+                'region'      => $config['region'],
+                'credentials' => [
+                    'key'    => $config['key'],
+                    'secret' => $config['secret'],
+                ],
+            ]);
 
-        $this->logGroup  = $config['log_group'];
-        $this->logStream = $config['log_stream'];
+            $this->logGroup  = $config['log_group'];
+            $this->logStream = $config['log_stream'];
+        } catch (AwsException $e) {
+            throw new \RuntimeException("Error al conectar con CloudWatch: " . $e->getMessage());
+        }
     }
 
     protected function write(\Monolog\LogRecord $record): void
@@ -43,14 +48,18 @@ class CloudWatchLogger extends AbstractProcessingHandler
             ],
         ];
 
-        if ($this->sequenceToken) {
-            $params['sequenceToken'] = $this->sequenceToken;
-        }
+        try {
+            if ($this->sequenceToken) {
+                $params['sequenceToken'] = $this->sequenceToken;
+            }
 
-        $result = $this->client->putLogEvents($params);
+            $result = $this->client->putLogEvents($params);
 
-        if (isset($result['nextSequenceToken'])) {
-            $this->sequenceToken = $result['nextSequenceToken'];
+            if (isset($result['nextSequenceToken'])) {
+                $this->sequenceToken = $result['nextSequenceToken'];
+            }
+        } catch (AwsException $e) {
+            throw new \RuntimeException("Error al enviar log a CloudWatch: " . $e->getMessage());
         }
     }
 }
